aa84baf2c157634ffa5a5b0585acb6ab1d5aff9a
Nasredene Elyamani <ne227@cornell.edu>
2020-10-23 00:12:44 -0700

    What I have so far.

---
+++ blokus/blokus/player.mli
@@ -0,0 +1,49 @@
+(** The abstract type of values representing player. *)
+type player
+
+(** The type of player's piece. *)
+type piece
+
+(** The type of game board. *)
+type game = {
+  gameboard: char array array
+}
+
+(** [placed_piece p] removes the piece that [p] represents.
+    Requires: [p] is a valid player piece representation present in 
+    the player's inventory. *)
+(*val placed_piece : piece -> player -> player*)
+
+(** [valid_moves v] is set of valid moves for a piece after a piece has 
+    been chosen by the player for each orientation possible
+    Requires: [p] is a valid player piece representation. *)
+(*val valid_moves : piece -> (int * int) -> game -> (int * int ) list*)
+
+(** [is_eliminated player] sees all the valid moves remaining for the 
+    remaining pieces. 
+    Returns: true if nothing and false if there are still moves.  *)
+(*val is_eliminated : piece -> bool*)
+
+(** [is_touching player game] sees that the placed piece touches just the 
+    corner of one of the pieces on the board and does not touch the faces 
+    of it’s other pieces. *)
+(*val is_touching : piece -> (int * int) -> game -> bool*)
+val corners_of_player_piece: bool array array->(int*int) list
+
+val check_i_zero: int -> bool array array-> (int*int) list
+
+val check_j_zero: int -> bool array array-> (int*int) list
+
+val check_any: int -> int -> bool array array-> (int*int) list
+
+val get_all_corners: bool array array -> (int*int) list
+
+val is_touching_simple: int * int -> game -> bool
+
+(*type player_piece
+
+  type game_board
+
+  val is_touching: (int * int) -> player_piece -> game_board -> bool
+
+  val valid_moves: (int * int) -> player_piece -> game_board -> (int * int) list*)
